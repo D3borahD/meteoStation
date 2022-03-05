f4bfe1f96dcae806fe9f3b2cee15975aad7be7d3
Deb <deborahdev.io@gmail.com>
2022-03-05 20:40:39 +0100

    mobile and desktop interface

--- index.php
+++ index.php
@@ -4,10 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Station météo</title>
 </head>
 <body>
-    
-    
+    <div class="main">
+    <nav class="sidebar">
+    <div class="brand">
+    <img src="styles/asset/logoDesktop.png" alt="logo">
+    <span id='brand'>ATMOS</span>
+</div>
+    </nav>
+            <div class="box clock">
+              <p>Lyon</p>
+              <span>17:09</span>
+            </div>
+        
+            <div class="box temperature">
+                <p>Température</p>
+                <span>28.7</span>
+                <p>°C</p>
+            </div>
+       
+            <div class="box humidity">
+                <p>Humidité</p>
+                <span>50</span>
+                <p>%</p>
+            </div>
+           
+            <div class="box graph">
+                <p>graph</p>
+                <img src="styles/asset/graph.jpg" alt="">
+            </div>
+
+  
+    </div>
 </body>
 </html>
+
